224a255c90efbd267f5da1d041409ab93a079267
Kate Senko <ksenko@arlo.com>
2019-10-10 17:12:53 +0300

    init test

--- src/main/java/com/example/chargestation/config/WebConfiguration.java
+++
@@ -1,4 +0,0 @@
-package com.example.chargestation.config;
-
-public class WebConfiguration {
-}
---
+++ src/test/groovy/com/example/chargestation/service/ChargeServiceImplTest.groovy
@@ -0,0 +1,9 @@
+package com.example.chargestation.service
+
+import spock.lang.Specification
+
+/**
+ *
+ */
+class ChargeServiceImplTest extends Specification {
+}
